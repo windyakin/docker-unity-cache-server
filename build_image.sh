9151fe685305db528e55d4d8c7142075c56a8305
windyakin <windyakin@gmail.com>
2018-11-24 15:56:20 +0900

    :put_litter_in_its_place: Remove LTS version support code

--- build_image.sh
+++ build_image.sh
@@ -2,7 +2,6 @@
 
 IMAGE_NAME=windyakin/docker-unity-cache-server
 VERSIONS=$(find -maxdepth 1 -type d | grep -E "^\./[0-9\.]+$" | sed -e "s/\.\///g" | sort)
-LTS_VERSION=$(cat ./LTSVersion)
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
@@ -11,11 +10,6 @@ for version in ${VERSIONS}; do
   cd "${TRAVIS_BUILD_DIR}/${version}"
   docker build --pull=true --no-cache=true --tag="${IMAGE_NAME}:${version}" .
   docker push "${IMAGE_NAME}:${version}"
-  if [ "${version}" = "${LTS_VERSION}" ]; then
-    echo "${version} is LTS version."
-    docker tag "${IMAGE_NAME}:${version}" "${IMAGE_NAME}:lts"
-    docker push "${IMAGE_NAME}:lts"
-  fi
   echo "Finish ${version} build and push"
   cd "${TRAVIS_BUILD_DIR}"
 done
