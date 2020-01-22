d9f3daa447ac127f7ea27e42d9eaef2daff276bc
windyakin <windyakin@pepabo.com>
2020-01-22 14:35:41 +0900

    :arrow_up: Login and push to Docker Hub when master push and schedule job only

--- .github/workflows/build_docker_image.sh
+++ .github/workflows/build_docker_image.sh
@@ -4,8 +4,6 @@ if [ -z ${GITHUB_WORKSPACE} ]; then
   GITHUB_WORKSPACE=$(cd $(dirname $0)/../../ && pwd)
 fi
 
-echo "${GITHUB_WORKSPACE}"
-
 IMAGE_NAME=windyakin/docker-unity-cache-server
 VERSIONS=$(cat << EOS
 $(find "${GITHUB_WORKSPACE}" -maxdepth 1 -type d | xargs -I FILEPATH basename FILEPATH | egrep "^[0-9]+\.[0-9]+\.[0-9]+([-+.][a-zA-Z0-9-]+)*$" | sed -e "s/\.\///g" | sort -n)
@@ -13,10 +11,8 @@ latest
 EOS
 )
 
-echo "Build versions list in this job:"
+echo "Build target versions list in this job:"
 echo "${VERSIONS}"
-
-# docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
 
 for version in ${VERSIONS}; do
   echo "Start ${IMAGE_NAME}:${version} image build and push to Docker Hub"
@@ -27,7 +23,12 @@ for version in ${VERSIONS}; do
   fi
   cd "${build_dir}"
   docker build --pull=true --no-cache=true --tag="${IMAGE_NAME}:${version}" .
-  # docker push "${IMAGE_NAME}:${version}"
+
+  if [ "${DOCKER_PUSH}" = "true" ] && [ -n "${DOCKER_USERNAME}" ] && [ -n "${DOCKER_PASSWORD}" ]; then
+    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+    docker push "${IMAGE_NAME}:${version}"
+  fi
+
   echo "Finish ${IMAGE_NAME}:${version} image build and push to Docker Hub"
   cd "${GITHUB_WORKSPACE}"
 done
